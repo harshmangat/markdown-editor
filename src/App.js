2f8846dfca044c8466e80b4c16945599691792f7
Harsh Mangat <harshmangat0007@gmail.com>
2022-10-06 12:22:05 +0530

    feat: create markdown-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import ReactMarkdown from "react-markdown";
+import { marked } from "marked";
+import "./App.css";
 
 function App() {
+  const [markdown, setMarkdown] = React.useState("# sup");
+
+  function handleChange(e) {
+    setMarkdown(e.target.value);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <textarea value={markdown} onChange={handleChange} />
+
+      <ReactMarkdown className="preview" children={markdown} />
+      
+      {/* <div className="preview"  dangerouslySetInnerHTML={{__html:marked.parse(markdown) }} /> */}
     </div>
   );
 }
